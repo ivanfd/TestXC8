c8020d7faf97b06202168191c537be168353f525
ivanfd <ivan_fd@i.ua>
2019-03-25 10:10:21 +0200

    test!

---
+++ main.c
@@ -0,0 +1,21 @@
+/*
+ * File:   main.c
+ * Author: Ivan
+ *
+ * Created on 25 марта 2019 г., 10:02
+ */
+
+
+#include <xc.h>
+#include "main.h"
+
+#define VAL 61536
+
+
+void main(void) {
+   uint8_t test;
+   
+   test = LOW_BYTE(VAL);
+    
+    return;
+}
